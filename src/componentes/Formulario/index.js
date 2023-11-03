f6d6f2e5f183ccfdc51059917e1f4dae13955c66
Milene <milenefaria33@gmail.com>
2023-11-02 21:13:47 -0300

    criacao do formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log('Salvando formulário')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para adicionar o colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome..." />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome..." />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo..." />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..." />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>            
+                <Botao>Adicionar</Botao>
             </form>
         </section>
     )
