89febf004af2a7523f6b40957a0866a9ce3ae870
Milene <milenefaria33@gmail.com>
2023-11-11 22:32:34 -0300

    cadastrando colaborador

--- src/App.js
+++ src/App.js
@@ -29,7 +29,12 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} onRegister={colaborador => addColaborador(colaborador)}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+      {times.map(time => <Time key={time.nome} 
+          nome={time.nome} 
+          corPrimaria={time.corPrimaria} 
+          corSecundaria={time.corSecundaria} 
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)}
     </div>
   );
 }
